36855ca33ae21464b677887e7a8441d96ac33d95
Rhian Adams <rhian.gwawr@gmail.com>
2022-01-14 20:12:26 +0000

    Added ItemPropertyUpdate class to Domain package

---
+++ src/Cadenza.Common/Cadenza.Domain/Models/ItemPropertyUpdate.cs
@@ -0,0 +1,21 @@
+﻿namespace Cadenza.Domain;
+
+public class ItemPropertyUpdate
+{
+    public ItemType ItemType { get; set; }
+    public string Id { get; set; }
+    public string Item { get; set; }
+    public ItemProperty Property { get; set; }
+    public string OriginalValue { get; set; }
+    public string UpdatedValue { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || !(obj is ItemPropertyUpdate update))
+            return false;
+
+        return update.ItemType == this.ItemType
+            && update.Id == this.Id
+            && update.Property == this.Property;
+    }
+}
