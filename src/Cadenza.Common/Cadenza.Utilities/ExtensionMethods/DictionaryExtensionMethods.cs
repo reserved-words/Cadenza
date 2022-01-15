41657cebaf97b26aa79a94734db142edd0d4278d
Rhian Adams <rhian.gwawr@gmail.com>
2022-01-15 17:36:10 +0000

    Reorganisation of Common library and added FullLibrary to Domain package

---
+++ src/Cadenza.Common/Cadenza.Utilities/ExtensionMethods/DictionaryExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿namespace Cadenza.Utilities;
+
+public static class DictionaryExtensionMethods
+{
+    public static T GetOrAdd<T>(this Dictionary<string, T> items, string key) where T : new()
+    {
+        var value = items.GetValueOrDefault(key);
+        if (value == null)
+        {
+            value = new T();
+            items.Add(key, value);
+        }
+        return value;
+    }
+
+    public static TValue ValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key)
+    {
+        return dictionary.TryGetValue(key, out TValue value)
+            ? value
+            : default;
+    }
+}
