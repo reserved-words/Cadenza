d47df0ac01b631e0f110470088e5e58932e59721
Rhian Adams <rhian.gwawr@gmail.com>
2022-02-09 18:29:47 +0000

    update

--- src/Cadenza.Common/Cadenza.Domain/Models/FullLibrary.cs
+++ src/Cadenza.Common/Cadenza.Domain/Models/FullLibrary.cs
@@ -2,11 +2,11 @@
 
 public class FullLibrary
 {
-    public List<ArtistInfo> Artists { get; set; }
+    public List<ArtistInfo> Artists { get; set; } = new();
 
-    public List<TrackInfo> Tracks { get; set; }
+    public List<TrackInfo> Tracks { get; set; } = new();
 
-    public List<AlbumInfo> Albums { get; set; }
+    public List<AlbumInfo> Albums { get; set; } = new();
 
-    public List<AlbumTrackLink> AlbumTrackLinks { get; set; }
+    public List<AlbumTrackLink> AlbumTrackLinks { get; set; } = new();
 }
