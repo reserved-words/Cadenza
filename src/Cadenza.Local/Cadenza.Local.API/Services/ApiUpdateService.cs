c0d49435495d3d3dc53c17309c72c827625e5991
Rhian Adams <rhian.gwawr@gmail.com>
2022-03-22 22:10:30 +0000

    Fixed issues with lyrics not being saved

--- src/Cadenza.Local/Cadenza.Local.API/Services/ApiUpdateService.cs
+++ src/Cadenza.Local/Cadenza.Local.API/Services/ApiUpdateService.cs
@@ -7,18 +7,37 @@ public class ApiUpdateService : IApiUpdateService
 {
     private readonly IFileUpdateService _id3UpdateQueue;
     private readonly ILibrary _library;
+    private readonly IAlbumCache _albumCache;
     private readonly IArtistCache _artistCache;
+    private readonly ITrackCache _trackCache;
 
-    public ApiUpdateService(IFileUpdateService updateQueue, ILibrary library, IArtistCache artistCache)
+    public ApiUpdateService(IFileUpdateService updateQueue, ILibrary library, IArtistCache artistCache, IAlbumCache albumCache, ITrackCache trackCache)
     {
         _id3UpdateQueue = updateQueue;
         _library = library;
         _artistCache = artistCache;
+        _albumCache = albumCache;
+        _trackCache = trackCache;
     }
 
     public async Task<FileUpdateQueue> GetUpdates()
     {
         return await _id3UpdateQueue.Get();
+    }
+
+    public async Task UpdateAlbum(AlbumUpdate update)
+    {
+        var album = await _albumCache.GetAlbum(update.Id);
+
+        if (album == null)
+            return;
+
+        await _albumCache.UpdateAlbum(update);
+        await _library.UpdateAlbum(update);
+        foreach (var updatedItem in update.Updates)
+        {
+            await _id3UpdateQueue.Add(updatedItem);
+        }
     }
 
     public async Task UpdateArtist(ArtistUpdate update)
@@ -30,6 +49,21 @@ public class ApiUpdateService : IApiUpdateService
 
         await _artistCache.UpdateArtist(update);
         await _library.UpdateArtist(update);
+        foreach (var updatedItem in update.Updates)
+        {
+            await _id3UpdateQueue.Add(updatedItem);
+        }
+    }
+
+    public async Task UpdateTrack(TrackUpdate update)
+    {
+        var track = await _trackCache.GetTrack(update.Id);
+
+        if (track == null)
+            return;
+
+        await _trackCache.UpdateTrack(update);
+        await _library.UpdateTrack(update);
         foreach (var updatedItem in update.Updates)
         {
             await _id3UpdateQueue.Add(updatedItem);
