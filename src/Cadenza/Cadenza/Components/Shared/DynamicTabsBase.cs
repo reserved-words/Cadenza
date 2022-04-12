884ed312bf99d1cabfc5a29b5aa20e5713dc2d8e
Rhian Adams <rhian.gwawr@gmail.com>
2022-04-12 22:44:29 +0100

    Fixed switching to newest tab

--- src/Cadenza/Cadenza/Components/Shared/DynamicTabsBase.cs
+++ src/Cadenza/Cadenza/Components/Shared/DynamicTabsBase.cs
@@ -17,11 +17,6 @@ public class DynamicTabsBase : ComponentBase
 
     private IEnumerable<DynamicTabsItem> _allItems => FixedItems.Concat(DynamicItems);
 
-    protected override void OnParametersSet()
-    {
-        // SetSelectedItem();
-    }
-
     protected void CloseTabCallback(MudTabPanel panel)
     {
         var itemId = panel.Tag.ToString();
@@ -44,61 +39,12 @@ public class DynamicTabsBase : ComponentBase
         {
             var selectIndex = allItems.IndexOf(item);
             SelectedTabIndex = selectIndex;
+            StateHasChanged();
         }
     }
 
-    //private void AddTab(DynamicTabsItem tab)
-    //{
-    //    if (DynamicItems.Any(t => t.Id == tab.Id))
-    //    {
-    //        ShowTab(tab.Id);
-    //    }
-    //    else
-    //    {
-    //        DynamicItems.Add(tab);
-    //        SwitchToTab = tab.Id;
-    //    }
-
-    //    StateHasChanged();
-    //}
-
     protected override void OnAfterRender(bool firstRender)
     {
-        //if (SwitchToTab != null)
-        //{
-        //    ShowTab(SwitchToTab);
-        //    SwitchToTab = null;
-        //}
         SetSelectedItem();
     }
-
-    //private void ShowTab(string id)
-    //{
-    //    var tab = DynamicItems.Single(t => t.Id == id);
-    //    var index = DynamicItems.IndexOf(tab);
-    //    var newIndex = FixedItems.Count + index;
-
-    //    if (SelectedTabIndex != newIndex)
-    //    {
-    //        SelectedTabIndex = newIndex;
-    //    }
-
-    //    StateHasChanged();
-    //}
-}
-
-public class DynamicTabsItem
-{
-    public DynamicTabsItem(string id, string title, string icon, Type component, Dictionary<string, object> parameters = null)
-    {
-        Id = id;
-        Title = title;
-        Icon = icon;
-        RenderFragment = component.RenderFragment(parameters);
-    }
-
-    public string Id { get; }
-    public string Icon { get; }
-    public string Title { get; }
-    public RenderFragment RenderFragment { get; }
 }
--- src/Cadenza/Cadenza/Pages/IndexBase.cs
+++ src/Cadenza/Cadenza/Pages/IndexBase.cs
@@ -1,10 +1,8 @@
 ﻿using Cadenza.Components.Shared;
 using Cadenza.Components.Tabs;
 using Cadenza.Core.App;
-using Cadenza.Core.CurrentlyPlaying;
 using Cadenza.Core.Model;
 using Cadenza.Interfaces;
-using Microsoft.AspNetCore.Components.Rendering;
 
 namespace Cadenza.Pages;
 
@@ -23,8 +21,8 @@ public class IndexBase : ComponentBase
 
     protected bool IsInitalised { get; private set; }
 
-    protected List<ViewItem> ItemTabs = new(); 
-    
+    protected List<ViewItem> ItemTabs = new();
+
     public List<DynamicTabsItem> FixedItems = new();
 
     public List<DynamicTabsItem> DynamicItems = new();
@@ -41,7 +39,6 @@ public class IndexBase : ComponentBase
             new DynamicTabsItem("System", "System", "fas fa-cog", typeof(SystemInfoTab))
         };
 
-        App.TrackStarted += App_TrackStarted;
         App.ItemRequested += App_ItemRequested;
 
         await OnStartup();
@@ -51,66 +48,25 @@ public class IndexBase : ComponentBase
     {
         if (!DynamicItems.Any(t => t.Id == e.Item.Id))
         {
-             DynamicItems.Add(new DynamicTabsItem(e.Item.Id, e.Item.Name, e.Item.Type.GetIcon(), typeof(ItemTab), new Dictionary<string, object>
-             {
-                 { "Item", e.Item },
-             }));
+            DynamicItems.Add(GetItemTab(e));
         }
 
         SelectedItem = e.Item.Id;
-
         StateHasChanged();
         return Task.CompletedTask;
     }
 
-    private Task App_TrackStarted(object sender, TrackEventArgs e)
+    private static DynamicTabsItem GetItemTab(ItemEventArgs e)
     {
-        //Playing = true;
-        //StateHasChanged();
-        return Task.CompletedTask;
+        return new DynamicTabsItem(e.Item.Id, e.Item.Name, e.Item.Type.GetIcon(), typeof(ItemTab), new Dictionary<string, object>
+        {
+            { "Item", e.Item },
+        });
     }
 
     protected async Task OnStartup()
     {
         var success = await DialogService.Run(() => ConnectService.GetStartupTasks(), "Connecting Services", true);
-
         IsInitalised = success;
     }
-
-    //protected void CloseTabCallback(MudTabPanel panel)
-    //{
-    //    var itemId = panel.Tag.ToString();
-    //    if (ItemTabs.Any(i => i.Id == itemId))
-    //    {
-    //        var tab = ItemTabs.Single(i => i.Id == itemId);
-    //        ItemTabs.Remove(tab);
-    //    }
-    //}
-
-    //protected override void OnAfterRender(bool firstRender)
-    //{
-    //    if (SwitchToTab != null)
-    //    {
-    //        ShowTab(SwitchToTab);
-    //        SwitchToTab = null;
-    //    }
-    //}
-
-    //private void ShowTab(string id)
-    //{
-    //    var tab = ItemTabs.Single(t => t.Id == id);
-    //    var index = ItemTabs.IndexOf(tab);
-    //    var newIndex = FixedTabCount + index;
-
-    //    if (SelectedTabIndex == newIndex)
-    //    {
-    //        // Display message
-    //    }
-    //    else
-    //    {
-    //        SelectedTabIndex = FixedTabCount + index;
-    //    }
-
-    //    StateHasChanged();
-    //}
 }
