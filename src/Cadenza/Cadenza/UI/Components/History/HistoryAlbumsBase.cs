fba41105fd81e588471a8dd95334459177de55b1
Rhian Adams <rhian.gwawr@gmail.com>
2022-05-06 10:48:24 +0100

    Missed from previous commit

---
+++ src/Cadenza/Cadenza/UI/Components/History/HistoryAlbumsBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Cadenza.UI.Components.History;
+
+public class HistoryAlbumsBase : HistoryDisplayBase<PlayedAlbum>
+{
+    protected override async Task<List<PlayedAlbum>> GetItems(HistoryPeriod period)
+    {
+        return (await History.GetPlayedAlbums(period, 5, 1)).ToList();
+    }
+}
---
+++ src/Cadenza/Cadenza/UI/Components/History/HistoryTracksBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Cadenza.UI.Components.History;
+
+public class HistoryTracksBase : HistoryDisplayBase<PlayedTrack>
+{
+    protected override async Task<List<PlayedTrack>> GetItems(HistoryPeriod period)
+    {
+        return (await History.GetPlayedTracks(period, 5, 1)).ToList();
+    }
+}
