5ceb22c894bb8c90d09549b3b88244fcf2e218db
Rhian Adams <rhian.gwawr@gmail.com>
2022-10-04 17:43:39 +0100

    Added detailed logging to PlayedFilesService

--- src/Cadenza.Local/Cadenza.Local.SyncService/Services/PlayedFilesService.cs
+++ src/Cadenza.Local/Cadenza.Local.SyncService/Services/PlayedFilesService.cs
@@ -1,18 +1,24 @@
-﻿namespace Cadenza.Local.SyncService.Services;
+﻿using Microsoft.Extensions.Logging;
+
+namespace Cadenza.Local.SyncService.Services;
 
 internal class PlayedFilesService : IService
 {
     private readonly IFileAccess _fileAccess;
+    private readonly ILogger<PlayedFilesService> _logger;
     private readonly IOptions<CurrentlyPlayingSettings> _settings;
 
-    public PlayedFilesService(IFileAccess fileAccess, IOptions<CurrentlyPlayingSettings> settings)
+    public PlayedFilesService(IFileAccess fileAccess, IOptions<CurrentlyPlayingSettings> settings, ILogger<PlayedFilesService> logger)
     {
         _fileAccess = fileAccess;
         _settings = settings;
+        _logger = logger;
     }
 
     public Task Run()
     {
+        _logger.LogInformation($"Checking for played files");
+
         var files = GetPlayedFiles();
 
         foreach (var file in files)
@@ -20,19 +26,26 @@ internal class PlayedFilesService : IService
             TryDeleteFile(file.Path);
         }
 
+        _logger.LogInformation($"Played files processed");
+
         return Task.CompletedTask;
     }
 
-    private IEnumerable<FileDetails> GetPlayedFiles()
+    private List<FileDetails> GetPlayedFiles()
     {
         var directory = Path.Combine(_settings.Value.BaseDirectory, _settings.Value.DirectoryName);
 
         var allFiles = _fileAccess.GetFiles(directory);
 
+        _logger.LogInformation($"{allFiles.Count} total files found");
+
         var allButLatestFile = allFiles
             .Where(f => Path.GetFileName(f.Path).StartsWith(_settings.Value.FilePrefix))
             .OrderByDescending(f => f.DateCreated)
-            .Skip(1);
+            .Skip(1)
+            .ToList();
+
+        _logger.LogInformation($"{allButLatestFile.Count} files to be deleted");
 
         return allButLatestFile;
     }
@@ -43,9 +56,9 @@ internal class PlayedFilesService : IService
         {
             _fileAccess.DeleteFile(path);
         }
-        catch (IOException)
+        catch (IOException ex)
         {
-            // skip - file is in use
+            _logger.LogWarning(ex, "File could not be deleted at this time");
         }
     }
 }
