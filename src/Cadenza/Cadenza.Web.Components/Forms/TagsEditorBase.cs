9a77cbcb874bfac52e8682800173d046ec6cf140
Rhian Adams <rhian.gwawr@gmail.com>
2022-10-26 18:18:05 +0100

    Added validation to tags

---
+++ src/Cadenza/Cadenza.Web.Components/Forms/TagsEditorBase.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Cadenza.Web.Components.Forms;
+
+public class TagsEditorBase : ComponentBase
+{
+    private const string RegexPattern = @"^[0-9a-zß-ÿ\-]*$"; 
+    
+    [Parameter]
+    public TagList Model { get; set; } = new TagList();
+
+    protected DefaultFocus DefaultFocus { get; set; }
+
+    protected string ErrorMessage { get; set; }
+
+    public void OnAddTag(string tag)
+    {
+        if (string.IsNullOrWhiteSpace(tag))
+        {
+            ErrorMessage = null;
+            return;
+        }
+
+        if (!Regex.IsMatch(tag, RegexPattern))
+        {
+            ErrorMessage = "Only aphanumerics and hyphens allowed";
+            DefaultFocus = DefaultFocus.FirstChild;
+            return;
+        }
+
+        ErrorMessage = null;
+        Model.Add(tag);
+        NewTagText = "";
+        DefaultFocus = DefaultFocus.FirstChild;
+    }
+
+    public void OnRemoveTag(MudChip chip)
+    {
+        Model.Remove(chip.Text);
+    }
+
+    public bool IsNewTagTextEmpty => string.IsNullOrWhiteSpace(NewTagText);
+
+    public string NewTagText { get; set; }
+}
