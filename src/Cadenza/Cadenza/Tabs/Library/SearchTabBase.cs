8f22f136547d67d244d3c445c003226e32aceadd
Rhian Adams <rhian.gwawr@gmail.com>
2022-10-28 22:37:29 +0100

    Started implementation of Search tab - display all tags

--- src/Cadenza/Cadenza/Tabs/Library/SearchTabBase.cs
+++ src/Cadenza/Cadenza/Tabs/Library/SearchTabBase.cs
@@ -1,5 +1,33 @@
-﻿namespace Cadenza.Tabs.Library;
+﻿using Cadenza.Web.Common.Interfaces.Searchbar;
+using Cadenza.Web.Common.Interfaces.View;
+
+namespace Cadenza.Tabs.Library;
 
 public class SearchTabBase : ComponentBase
 {
+    [Inject]
+    public ISearchCache Cache { get; set; }
+
+    [Inject]
+    public IMessenger Messenger { get; set; }
+
+    [Inject]
+    public IItemViewer Viewer { get; set; }
+
+    protected List<PlayerItem> Results { get; set; } = new List<PlayerItem>();
+
+    protected async Task OnSearch()
+    {
+        Results = Cache.Items
+            // .Where(x => x.Name != null && x.Name.Contains(value, StringComparison.InvariantCultureIgnoreCase))
+            .Where(x => x.Type == PlayerItemType.Tag && !string.IsNullOrWhiteSpace(x.Name))
+            .OrderBy(x => x.Type)
+            .ThenBy(x => x.Name)
+            .ToList();
+    }
+
+    protected async Task OnViewItem(PlayerItem item)
+    {
+        await Viewer.ViewSearchResult(item);
+    }
 }
