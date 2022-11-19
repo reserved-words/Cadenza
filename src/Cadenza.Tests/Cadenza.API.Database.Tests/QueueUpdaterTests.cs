834cfd54682d9ea62ce46ea5de2df56c023262d4
Rhian Adams <rhian.gwawr@gmail.com>
2022-11-19 19:35:53 +0000

    Added more tests for QueueUpdater

--- src/Cadenza.Tests/Cadenza.API.Database.Tests/QueueUpdaterTests.cs
+++ src/Cadenza.Tests/Cadenza.API.Database.Tests/QueueUpdaterTests.cs
@@ -1,4 +1,5 @@
 using Cadenza.API.Database.Services.Updaters;
+using Cadenza.Common.Domain.Enums;
 using Cadenza.Common.Domain.Model.Updates;
 using NUnit.Framework;
 using System.Collections.Generic;
@@ -22,5 +23,43 @@ public class QueueUpdaterTests
 		Assert.That(queue, Is.Not.Null);
 		Assert.That(queue, Has.Count.EqualTo(1));
 		Assert.That(queue, Contains.Item(update));
+	}
+
+	[Test]
+	public void Queue_GivenQueuedItem_IsDifferentType_AddsUpdate()
+	{
+		// Arrange
+		var queuedUpdate = new EditedItem { Type = LibraryItemType.Artist, Id = "abc" };
+		var newUpdate = new EditedItem { Type = LibraryItemType.Track, Id = "abc" };
+		var queue = new List<EditedItem> { queuedUpdate };
+		var sut = new QueueUpdater();
+
+		// Act
+		sut.Queue(queue, newUpdate);
+
+		// Assert
+		Assert.That(queue, Is.Not.Null);
+		Assert.That(queue, Has.Count.EqualTo(2));
+		Assert.That(queue, Contains.Item(queuedUpdate));
+		Assert.That(queue, Contains.Item(newUpdate));
+	}
+
+	[Test]
+	public void Queue_GivenQueuedItem_HasDifferentId_AddsUpdate()
+	{
+		// Arrange
+		var queuedUpdate = new EditedItem { Type = LibraryItemType.Artist, Id = "abc" };
+		var newUpdate = new EditedItem { Type = LibraryItemType.Artist, Id = "def" };
+		var queue = new List<EditedItem> { queuedUpdate };
+		var sut = new QueueUpdater();
+
+		// Act
+		sut.Queue(queue, newUpdate);
+
+		// Assert
+		Assert.That(queue, Is.Not.Null);
+		Assert.That(queue, Has.Count.EqualTo(2));
+		Assert.That(queue, Contains.Item(queuedUpdate));
+		Assert.That(queue, Contains.Item(newUpdate));
 	}
 }
