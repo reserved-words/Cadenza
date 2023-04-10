15f77d52beb3c095f14c915cea3787d9366d7820
Rhian Adams <rhian.gwawr@gmail.com>
2023-04-10 15:11:39 +0100

    Added views and stored procedure for returning recently played items

---
+++ src/SQL/Cadenza.Sql/Views/History/vw_MostRecentPlays.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [History].[vw_MostRecentPlays]
+AS
+SELECT 
+	[PlaylistTypeId], 
+	[PlaylistItemId], 
+	MAX([PlayedAt]) [PlayedAt]
+FROM
+	[History].[vw_PlayedItems]
+GROUP BY
+	[PlaylistTypeId], 
+	[PlaylistItemId]
