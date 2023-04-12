64084916794ae285c53872df1d49434049d6efe2
Rhian Adams <rhian.gwawr@gmail.com>
2023-04-12 21:20:48 +0100

    Icon is now displayed when the currently playing track is in the currently displayed tracklist

--- src/Cadenza/Cadenza.Web.Common/Enums/StoreKey.cs
+++ src/Cadenza/Cadenza.Web.Common/Enums/StoreKey.cs
@@ -5,5 +5,6 @@ public enum StoreKey
     LastFmSessionKey, // infinite lifespan - but can be revoked
     LastFmToken, // 60 minute lifespan
     CurrentTrack,
+    CurrentTrackId,
     CurrentTrackSource
 }
