82b961daee3ced8f261c391d8a9501a2af45cb05
Rhian Adams <rhian.gwawr@gmail.com>
2023-04-18 21:08:12 +0100

    Added authentication

--- src/Cadenza/Cadenza/Program.cs
+++ src/Cadenza/Cadenza/Program.cs
@@ -1,5 +1,7 @@
 namespace Cadenza;
 
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 public class Program
 {
     public static async Task Main(string[] args)
@@ -8,6 +10,12 @@ public class Program
             .RegisterComponents()
             .RegisterDependencies()
             .RegisterConfiguration();
+
+        builder.Services.AddOidcAuthentication(options =>
+        {
+            builder.Configuration.Bind("Authentication", options.ProviderOptions);
+            options.ProviderOptions.ResponseType = "code";
+        });
 
         await builder.Build().RunAsync();
     }
