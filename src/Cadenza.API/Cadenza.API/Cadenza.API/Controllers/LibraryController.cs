f14ff04af6f2976862b05b828a4b6db494dc296b
Rhian Adams <rhian.gwawr@gmail.com>
2023-05-27 11:20:43 +0100

    Fixed errors with fetching API calls with ID

--- src/Cadenza.API/Cadenza.API/Cadenza.API/Controllers/LibraryController.cs
+++ src/Cadenza.API/Cadenza.API/Cadenza.API/Controllers/LibraryController.cs
@@ -11,31 +11,31 @@ public class LibraryController : ControllerBase
         _service = service;
     }
 
-    [HttpGet("Album")]
+    [HttpGet("Album/{id}")]
     public async Task<AlbumInfo> Album(int id)
     {
         return await _service.Album(id);
     }
 
-    [HttpGet("Album/Tracks")]
+    [HttpGet("Album/Tracks/{id}")]
     public async Task<List<AlbumTrack>> AlbumTracks(int id)
     {
         return await _service.AlbumTracks(id);
     }
 
-    [HttpGet("Artist")]
+    [HttpGet("Artist/{id}")]
     public async Task<ArtistInfo> Artist(int id)
     {
         return await _service.Artist(id);
     }
 
-    [HttpGet("Artist/Albums")]
+    [HttpGet("Artist/Albums/{id}")]
     public async Task<List<Album>> ArtistAlbums(int id)
     {
         return await _service.ArtistAlbums(id);
     }
 
-    [HttpGet("Artist/Tracks")]
+    [HttpGet("Artist/Tracks/{id}")]
     public async Task<List<Track>> ArtistTracks(int id)
     {
         return await _service.ArtistTracks(id);
@@ -47,25 +47,25 @@ public class LibraryController : ControllerBase
         return await _service.Artists();
     }
 
-    [HttpGet("Artists/Genre")]
+    [HttpGet("Artists/Genre/{id}")]
     public async Task<List<Artist>> GenreArtists(string id)
     {
         return await _service.GenreArtists(id);
     }
 
-    [HttpGet("Artists/Grouping")]
+    [HttpGet("Artists/Grouping/{id}")]
     public async Task<List<Artist>> GroupingArtists(Grouping id)
     {
         return await _service.GroupingArtists(id);
     }
 
-    [HttpGet("Tag")]
+    [HttpGet("Tag/{id}")]
     public async Task<List<PlayerItem>> Tag(string id)
     {
         return await _service.Tag(id);
     }
 
-    [HttpGet("Track")]
+    [HttpGet("Track/{id}")]
     public async Task<TrackFull> Track(int id)
     {
         return await _service.Track(id);
