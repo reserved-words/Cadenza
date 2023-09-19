b72b83c5a4b7526e79e7c6c87b75c6e495e54fd8
Rhian Adams <rhian.gwawr@gmail.com>
2023-09-19 11:56:07 +0100

    Removed unused interfaces and classes

--- src/Cadenza/Cadenza.Web.Common/Interfaces/Play/IPlayCoordinator.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace Cadenza.Web.Common.Interfaces.Play;
-
-//public interface IPlayCoordinator
-//{
-//    Task StopCurrentPlaylist();
-//    Task Play(PlaylistDefinition playlistDefinition);
-//}
