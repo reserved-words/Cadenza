05db32e65784120e648b875b0629bdef4d9c5c1c
Rhian Adams <rhian.gwawr@gmail.com>
2023-11-28 20:39:25 +0000

    Added stored procedures for dashboard summaries

---
+++ src/Cadenza.Database/Cadenza.Sql/Stored Procedures/History/GetTopArtists.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE [History].[GetTopArtists]
+	@HistoryPeriod INT,
+	@MaxItems INT
+AS
+BEGIN
+
+	DECLARE @StartTime DATETIME = [History].[GetStartDate](@HistoryPeriod);
+
+	WITH [Scrobbles] AS (
+		SELECT 
+			VTR.[ArtistId], COUNT(VSC.[ScrobbledAt]) [Count]
+		FROM 
+			[History].[vw_Scrobbles] VSC
+		INNER JOIN
+			[Library].[vw_TrackDetails] VTR ON VTR.[TrackId] = VSC.[TrackId]
+		WHERE
+			(@StartTime IS NULL OR VSC.[ScrobbledAt] >= @StartTime)
+		GROUP BY
+			VTR.[ArtistId]
+	)
+	SELECT TOP (@MaxItems)
+		ART.[Id],
+		ART.[Name],
+		SCR.[Count] [Plays]
+	FROM
+		[Scrobbles] SCR
+	INNER JOIN
+		[Library].[Artists] ART ON ART.[Id] = SCR.[ArtistId]
+	ORDER BY
+		SCR.[Count] DESC
+
+END
---
+++ src/Cadenza.Database/Cadenza.Sql/Views/History/vw_Scrobbles.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [History].[vw_Scrobbles]
+AS
+SELECT 
+	SCR.[ScrobbledAt],
+	TSC.[TrackId]
+FROM 
+	[History].[Scrobbles] SCR
+INNER JOIN
+	[History].[TrackScrobbles] TSC ON TSC.[ScrobbleId] = SCR.[Id]
+
