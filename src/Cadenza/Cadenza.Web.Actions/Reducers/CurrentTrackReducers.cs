877e099e18dcd18aab66dc4cac9c1c0d297b7175
Rhian Adams <rhian.gwawr@gmail.com>
2023-12-11 21:12:17 +0000

    Added reducers to update current track state when album tracks and artist releases are updated

--- src/Cadenza/Cadenza.Web.Actions/Reducers/CurrentTrackReducers.cs
+++ src/Cadenza/Cadenza.Web.Actions/Reducers/CurrentTrackReducers.cs
@@ -22,19 +22,17 @@ public static class CurrentTrackReducers
         if (state.Track == null || state.Track.Album.Id != action.AlbumId)
             return state;
 
-        var updatedAlbum = state.Track.Album with
-        {
-            Title = action.UpdatedAlbum.Title,
-            ReleaseType = action.UpdatedAlbum.ReleaseType,
-            Year = action.UpdatedAlbum.Year,
-            DiscCount = action.UpdatedAlbum.DiscCount,
-            ArtworkBase64 = action.UpdatedAlbum.ArtworkBase64,
-            Tags = action.UpdatedAlbum.Tags
+        return state with 
+        { 
+            Track = state.Track with
+            {
+                Album = action.UpdatedAlbum,
+                AlbumTrack = state.Track.AlbumTrack with
+                {
+                    DiscCount = action.UpdatedAlbum.DiscCount
+                }
+            } 
         };
-
-        var track = state.Track with { Album = updatedAlbum };
-
-        return state with { Track = track };
     }
 
     [ReducerMethod]
@@ -70,5 +68,59 @@ public static class CurrentTrackReducers
         var track = state.Track with { Track = action.UpdatedTrack };
 
         return state with { Track = track };
+    }
+
+    [ReducerMethod]
+    public static CurrentTrackState ReduceAlbumTracksUpdatedAction(CurrentTrackState state, AlbumTracksUpdatedAction action)
+    {
+        if (state.Track == null)
+            return state;
+
+        var updatedTrack = action.UpdatedAlbumTracks.SingleOrDefault(t => t.TrackId == state.Track.Id);
+
+        if (updatedTrack == null)
+            return state;
+
+        return state with
+        {
+            Track = state.Track with
+            {
+                Track = state.Track.Track with
+                {
+                    Title = updatedTrack.Title                     
+                },
+                AlbumTrack = state.Track.AlbumTrack with
+                {
+                    TrackNo = updatedTrack.TrackNo,
+                    DiscNo = updatedTrack.DiscNo,
+                    TrackCount = updatedTrack.DiscTrackCount
+                }
+            }
+        };
+    }
+
+    [ReducerMethod]
+    public static CurrentTrackState ReduceArtistReleasesUpdatedAction(CurrentTrackState state, ArtistReleasesUpdatedAction action)
+    {
+        if (state.Track == null)
+            return state;
+
+        var updatedAlbum = action.UpdatedArtistReleases.SingleOrDefault(a => a.Id == state.Track.Album.Id);
+
+        if (updatedAlbum == null)
+            return state;
+
+        return state with
+        {
+            Track = state.Track with
+            {
+                Album = state.Track.Album with
+                {
+                    Title = updatedAlbum.Title,
+                    ReleaseType = updatedAlbum.ReleaseType,
+                    Year = updatedAlbum.Year
+                }
+            }
+        };
     }
 }
