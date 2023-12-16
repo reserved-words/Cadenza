00326300163b0b86dee145a29efafb5a011c1224
Rhian Adams <rhian.gwawr@gmail.com>
2023-12-16 10:16:42 +0000

    Added album duration to Album page

--- src/Cadenza/Cadenza.Web.Components/Tabs/Library/AlbumTabBase.cs
+++ src/Cadenza/Cadenza.Web.Components/Tabs/Library/AlbumTabBase.cs
@@ -7,4 +7,6 @@ public class AlbumTabBase : FluxorComponent
     public bool Loading => ViewAlbumState.Value.IsLoading;
     public AlbumDetailsVM Album => ViewAlbumState.Value.Album;
     public IReadOnlyCollection<AlbumDiscVM> Discs => ViewAlbumState.Value.Tracks;
+
+    public int DurationSeconds => Discs.SelectMany(d => d.Tracks).Sum(t => t.DurationSeconds);
 }
