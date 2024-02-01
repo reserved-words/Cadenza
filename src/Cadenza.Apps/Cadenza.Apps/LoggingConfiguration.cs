17e06a3657927af4e9bc0760a16f6be174cd0712
Rhian Adams <rhian.gwawr@gmail.com>
2024-02-01 19:39:42 +0000

    Added second laptop deployment stage, moved logging config out of Serilog config section

--- src/Cadenza.Apps/Cadenza.Apps/LoggingConfiguration.cs
+++ src/Cadenza.Apps/Cadenza.Apps/LoggingConfiguration.cs
@@ -6,7 +6,7 @@ public static class LoggingConfiguration
 {
     public static string LogFilePath(this IConfiguration config)
     {
-        var loggingConfig = config.GetSection("Serilog");
+        var loggingConfig = config.GetSection("Logs");
         var logPathFormat = loggingConfig.GetValue<string>("LogPathFormat") ?? "";
         var applicationName = loggingConfig.GetValue<string>("ApplicationName");
         var logFilePath = string.Format(logPathFormat, applicationName);
