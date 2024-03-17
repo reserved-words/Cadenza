2445f326f66dc198481d2816bec55c1fd9264cef
Rhian Adams <rhian.gwawr@gmail.com>
2024-03-17 16:06:05 +0000

    Added headers to dashboard tabs on mobile view

--- src/Cadenza/Cadenza/Features/Tabs/Dashboard/Components/RecentlyAddedAlbums.razor.cs
+++ src/Cadenza/Cadenza/Features/Tabs/Dashboard/Components/RecentlyAddedAlbums.razor.cs
@@ -4,6 +4,8 @@ public class RecentlyAddedAlbumsBase : FluxorComponent
 {
     [Inject] public IState<HistoryRecentlyAddedAlbumsState> State { get; set; }
 
+    [Parameter] public bool ShowHeader { get; set; } = true;
+
     protected IReadOnlyCollection<RecentAlbumVM> RecentAlbums => State.Value.Albums;
 
     protected bool IsLoading => State.Value.IsLoading;
--- src/Cadenza/Cadenza/Features/Tabs/Dashboard/Components/RecentlyPlayedAlbums.razor.cs
+++ src/Cadenza/Cadenza/Features/Tabs/Dashboard/Components/RecentlyPlayedAlbums.razor.cs
@@ -4,6 +4,8 @@ public class RecentlyPlayedAlbumsBase : FluxorComponent
 {
     [Inject] public IState<HistoryRecentlyPlayedAlbumsState> State { get; set; }
 
+    [Parameter] public bool ShowHeader { get; set; } = true;
+
     protected IReadOnlyCollection<RecentAlbumVM> RecentAlbums => State.Value.Items;
 
     protected bool IsLoading => State.Value.IsLoading;
