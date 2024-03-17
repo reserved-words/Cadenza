d94881edca315d41b4a7d89f2de13828c863a989
Rhian Adams <rhian.gwawr@gmail.com>
2024-03-17 15:17:18 +0000

    Display 12 recently added albums instead of 8

--- src/Cadenza/Cadenza.Web.Common/Constants.cs
+++ src/Cadenza/Cadenza.Web.Common/Constants.cs
@@ -2,7 +2,7 @@
 
 public static class Constants
 {
-    public const int MaxRecentlyAddedAlbums = 8;
+    public const int MaxRecentlyAddedAlbums = 12;
     public const int MaxRecentlyPlayedAlbums = 12;
     public const int MaxRecentlyPlayedTracks = 10;
     public const int MaxTopPlayedAlbums = 5;
